6472ad669312921ff4681aee6fb014e10b863a4d
DESKTOP-1GA12D1\DELL <sanwailwin21@gmail.com>
2025-12-23 13:51:53 +0700

    - add account repo

---
+++ accounts/src/main/java/com/swl/accounts/service/IAccountsService.java
@@ -0,0 +1,15 @@
+package com.swl.accounts.service;
+
+import com.swl.accounts.dto.CustomerDto;
+
+public interface IAccountsService {
+
+    void createAccount(CustomerDto customerDto);
+
+    CustomerDto fetchAccount(String mobileNumber);
+
+    boolean updateAccount(CustomerDto customerDto);
+
+    boolean deleteAccount(String mobileNumber);
+
+}
